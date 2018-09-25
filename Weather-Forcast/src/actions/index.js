a7422dc70a13f050bc5f19e77c59a38909bde585
Josh Westbury <josh.westbury@gmail.com>
2018-09-24 21:37:03 -0500

    add axios request in actions/index.js

--- Weather-Forcast/src/actions/index.js
+++ Weather-Forcast/src/actions/index.js
@@ -1,1 +1,13 @@
 import * as key from "../api_key";
+const ROOT_URL = 'https://api.openweathermap.org/data/2.5/forecast?';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}id=${city}&APPID=${key}`;
+
+    i
+    return {
+        type: FETCH_WEATHER
+    }
+}
