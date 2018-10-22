827774e65cc4e41e7014c8040ab924a9c9bd58f0
Josh Westbury <josh.westbury@gmail.com>
2018-10-21 19:31:41 -0500

    add pass state to reducer

--- Weather-Forcast/src/actions/index.js
+++ Weather-Forcast/src/actions/index.js
@@ -1,11 +1,12 @@
 import { API_KEY } from "../../api_key";
 import * as axios from "axios";
-const ROOT_URL = "https://api.openweathermap.org/data/2.5/forecast?";
+
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}q=${city},us&appid=${API_KEY}`;
+  const url = `${ROOT_URL}q=${city},us&APPID=${API_KEY}`;
   const request = axios.get(url);
 
   return {
